2d8e95fca682431b0fc82c618d56bfa55077b6d4
Roman Kuzneckiy <r.kuzneckiy@geoscan.lan>
2018-11-23 10:16:02 +0300

    test evrthng

--- conf.py
+++ conf.py
@@ -39,6 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -74,7 +75,10 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+def setup(app):
+    app.add_stylesheet('style.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
