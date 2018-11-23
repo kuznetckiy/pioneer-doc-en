79f6d8fd871c0e5222c29e095f96dfa62d68a94c
Roman Kuzneckiy <r.kuzneckiy@geoscan.lan>
2018-11-23 13:02:17 +0300

    ltd

--- conf.py
+++ conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Pioneer'
-copyright = '2018, Geoscan ltd'
-author = 'Geoscan ltd'
+copyright = '2018, Geoscan LTD'
+author = 'Geoscan LTD'
 
 # The short X.Y version
 version = ''
